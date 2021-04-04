577988c27c9feea8a34a22e530d6648abb0606fe
Zarak <7254237+zarak@users.noreply.github.com>
2021-04-04 08:02:44 +0500

    Add zlib dependency so cabal repl works in shell

--- default.nix
+++ default.nix
@@ -1,3 +1,30 @@
-{ pkgs ? import <nixpkgs> { } }:
+let
+  config = 
+  {
+    packageOverrides = pkgs: 
+    {
+      haskellPackages = pkgs.haskellPackages.override 
+      {
+        overrides = new: old:
+        {
+          latex-parser = new.callPackage ./latex-parser.nix;
+        };
+      };
+    };
+  };
 
-pkgs.haskellPackages.callPackage ./latex-parser.nix { }
+  pkgs = import <nixpkgs> { inherit config; };
+
+  ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs: with hpkgs;
+    [ cabal-install
+      latex-parser
+      zlib
+    ]
+  );
+in
+
+with pkgs;
+
+mkShell 
+{ buildInputs = [ ghc ];
+}
