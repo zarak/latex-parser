eec2006695e1047b83dd81964e7010be8126c61f
Zarak <7254237+zarak@users.noreply.github.com>
2021-04-04 08:40:12 +0500

    Remove anki-tools package

--- latex-parser.nix
+++ latex-parser.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, aeson, anki-tools, base, HaTeX, lib, req }:
+{ mkDerivation, aeson, base, HaTeX, lib, req }:
 mkDerivation {
   pname = "latex-parser";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ aeson anki-tools base HaTeX req ];
+  libraryHaskellDepends = [ aeson base HaTeX req ];
   executableHaskellDepends = [ base ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
