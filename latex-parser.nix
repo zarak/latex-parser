d530c3153ce2e0d4bc2f1f4827cb026271dff9e1
Zarak <7254237+zarak@users.noreply.github.com>
2021-04-04 07:49:02 +0500

    Add aeson

--- latex-parser.nix
+++ latex-parser.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, anki-tools, base, HaTeX, lib, req }:
+{ mkDerivation, aeson, anki-tools, base, HaTeX, lib, req }:
 mkDerivation {
   pname = "latex-parser";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ anki-tools base HaTeX req ];
+  libraryHaskellDepends = [ aeson anki-tools base HaTeX req ];
   executableHaskellDepends = [ base ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module MyLib (someFunc) where
 
 import Text.LaTeX.Base.Parser (parseLaTeX, parseLaTeXFile)
 import Text.LaTeX.Base.Syntax (lookForEnv)
+import Data.Aeson
+import Network.HTTP.Req
+import Control.Monad.IO.Class (liftIO)
 
 someFunc :: IO ()
 someFunc = do
@@ -9,3 +13,22 @@ someFunc = do
   case res of
     Left e -> print e
     Right latex -> print $ lookForEnv "equation" latex
+
+
+getDeckNames :: IO ()
+getDeckNames = runReq defaultHttpConfig $ do
+    let payload =
+          object
+          [ "action" .= ("decknames" :: String)
+          , "version" .= (6 :: Int)
+          ]
+
+    r <-
+      req
+      POST
+      (http "localhost:8765")
+      (ReqBodyJson payload)
+      jsonResponse
+      mempty
+    liftIO $ print (responseBody r :: Value)
+
