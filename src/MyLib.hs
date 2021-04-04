f14a09f892b4b7f3a44a43e8eb00c6ffd304c69d
Zarak <7254237+zarak@users.noreply.github.com>
2021-04-04 10:49:21 +0500

    Create definition parser

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -6,14 +6,23 @@ import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
 import Network.HTTP.Req
 import Text.LaTeX.Base.Parser (parseLaTeX, parseLaTeXFile)
-import Text.LaTeX.Base.Syntax (lookForEnv)
+import Text.LaTeX.Base.Syntax (lookForEnv, LaTeX, TeXArg)
+
+
+parseDefinition :: [([TeXArg], LaTeX)] -> IO (String, String)
+parseDefinition input = do
+  print (head input)
+  pure ("A", "B")
 
 someFunc :: IO ()
 someFunc = do
   res <- parseLaTeXFile "test.tex"
   case res of
     Left e -> print e
-    Right latex -> print $ lookForEnv "equation" latex
+    Right latex -> do
+      -- print $ lookForEnv "equation" latex
+      x <- parseDefinition $ lookForEnv "definition" latex
+      print x
 
 getDeckNames :: IO ()
 getDeckNames = runReq defaultHttpConfig $ do
